fd44448f53d4faef123b1cd902d54929fd3fd64a
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2022-06-10 14:08:41 -0500

    <mypy>[]: <Advertise we use type annotations 🗂>
    [Bump patch version]
    
    []

--- setup.py
+++ setup.py
@@ -9,12 +9,21 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="ogl",
-    version="0.52",
+    version="0.53.1",
     author_email='Humberto.A.Sanchez.II@gmail.com',
     description='External Pyut Graphical Shapes',
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/hasii2011/pyutgraphicalmodel",
+    url="https://github.com/hasii2011/ogl",
+    package_data={
+        'miniogl':        ['py.typed'],
+        'ogl':             ['py.typed'],
+        'ogl.events':      ['py.typed'],
+        'ogl.preferences': ['py.typed'],
+        'ogl.resources':   ['py.typed'],
+        'ogl.sd':          ['py.typed'],
+    },
+
     packages=[
         'miniogl',
         'ogl',
