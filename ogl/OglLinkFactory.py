70a4510fe2b2dfeaa043e1c56dee5ae8238ef3d1
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2023-09-24 19:58:07 -0500

    <>[]: <Fix a blunder>
    []
    
    [https://github.com/hasii2011/ogl/issues/82]

--- ogl/OglLinkFactory.py
+++ ogl/OglLinkFactory.py
@@ -2,7 +2,7 @@
 from logging import Logger
 from logging import getLogger
 
-from hasiihelper.Singleton import Singleton
+from codeallybasic.Singleton import Singleton
 
 from pyutmodel.PyutLinkType import PyutLinkType
 
