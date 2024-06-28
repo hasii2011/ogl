fa1ade2ad9f94ea633034f4b9e9acfeabef69292
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2024-06-28 07:48:29 -0500

    <Refactor>[VShapes]: <Secret work XV!>
    [
    Start work more robust demo
    ]
    
    [https://github.com/hasii2011/ogl/issues/114]

--- tests/demo/rotatable/DemoRotatableShapes.py
+++ tests/demo/rotatable/DemoRotatableShapes.py
@@ -6,15 +6,24 @@ from logging import Logger
 from logging import getLogger
 
 from wx import App
+from wx import Bitmap
+from wx import BitmapBundle
 from wx import CommandEvent
 from wx import DEFAULT_FRAME_STYLE
 from wx import EVT_MENU
 from wx import FRAME_FLOAT_ON_PARENT
+from wx import FRAME_TOOL_WINDOW
 from wx import ID_EXIT
+from wx import ITEM_NORMAL
 from wx import Menu
 from wx import MenuBar
+from wx import NO_BORDER
 
 from wx import NewIdRef as wxNewIdRef
+from wx import NullBitmap
+from wx import TB_FLAT
+from wx import WindowIDRef
+from wx import ToolBar
 
 from wx.lib.sized_controls import SizedFrame
 from wx.lib.sized_controls import SizedPanel
@@ -60,7 +69,7 @@ class DemoRotatableShapes(App):
 
     def OnInit(self):
 
-        frameStyle: int = DEFAULT_FRAME_STYLE | FRAME_FLOAT_ON_PARENT
+        frameStyle: int = DEFAULT_FRAME_STYLE | FRAME_FLOAT_ON_PARENT | FRAME_TOOL_WINDOW
 
         self._frame = SizedFrame(parent=None, title="Test Rotatable Shapes", size=(FRAME_WIDTH, FRAME_HEIGHT), style=frameStyle)
         self._frame.CreateStatusBar()  # should always do this when there's a resize border
@@ -76,7 +85,7 @@ class DemoRotatableShapes(App):
         self._diagramFrame.diagram = self._diagram
 
         self._createApplicationMenuBar()
-
+        self._createToolBar()
         self.SetTopWindow(self._frame)
 
         self._frame.SetAutoLayout(True)
@@ -143,6 +152,43 @@ class DemoRotatableShapes(App):
         self._x += INCREMENT_X
         self._y += INCREMENT_Y
         return x, y
+
+    def _createToolBar(self):
+
+        from wx import TB_HORIZONTAL
+        from tests.demo.rotatable.icons.redCircle32 import embeddedImage as ImgCircle
+        from tests.demo.rotatable.icons.ellipse32 import embeddedImage as ImgEllipse
+        from tests.demo.rotatable.icons.polygon64 import embeddedImage as ImgPolygon
+        from tests.demo.rotatable.icons.arc32 import embeddedImage as ImgArc
+
+        nullBmp = BitmapBundle(NullBitmap)
+
+        tb: ToolBar = self._frame.CreateToolBar(TB_HORIZONTAL | NO_BORDER | TB_FLAT)
+
+        tb.SetSize((32, 32))
+
+        """
+        AddTool(toolId, label, bitmap, bmpDisabled, kind=ITEM_NORMAL, shortHelp=EmptyString, longHelp=EmptyString, clientData=None)
+        """
+        toolCircle: Bitmap      = ImgCircle.Bitmap
+        circleId:   WindowIDRef = wxNewIdRef()
+        tb.AddTool(circleId, 'Circle', toolCircle, nullBmp, ITEM_NORMAL, 'Circle', 'Long Help', 'Circle')
+
+        toolEllipse: Bitmap      = ImgEllipse.Bitmap
+        ellipseId:   WindowIDRef = wxNewIdRef()
+        tb.AddTool(ellipseId, 'Ellipse', toolEllipse, nullBmp, ITEM_NORMAL, 'Ellipse', 'Long Help', 'Ellipse')
+
+        toolPolygon: Bitmap      = ImgPolygon.Bitmap
+        polygonId:   WindowIDRef = wxNewIdRef()
+        tb.AddTool(polygonId, 'Polygon', toolPolygon, nullBmp, ITEM_NORMAL, 'Polygon', 'Long Help', 'Polygon')
+
+        toolArc: Bitmap      = ImgArc.Bitmap
+        arcId:   WindowIDRef = wxNewIdRef()
+        tb.AddTool(arcId, 'Arc', toolArc, nullBmp, ITEM_NORMAL, 'Arc', 'Long Help', 'Arc')
+
+        tb.AddSeparator()
+        self._frame.SetToolBar(tb)
+        tb.Realize()
 
 
 if __name__ == '__main__':
